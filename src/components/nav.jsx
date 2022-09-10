f6426a4b6be091e84c27ee54587b7bc900ba4770
YuvCarl <carldrake969@gmail.com>
2022-09-10 22:06:27 +0100

    first commit

---
+++ src/components/nav.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+        <nav className='py-50 bg-[black]' >
+            <span>CODEwithCREST</span>
+            <button >Contact</button>
+        </nav>
+
+            <div className="">
+
+            </div>
+
+    </div>
+  )
+}
